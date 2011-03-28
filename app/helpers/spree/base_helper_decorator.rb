260f2cfc92e7b9e0ddc4a985006ffed85de14228
Alexander Shuhin <a.medium@gmail.com>
2011-03-28 19:13:49 +0400

    Корзина в заголовке отображает только число товаров

---
+++ app/helpers/spree/base_helper_decorator.rb
@@ -0,0 +1,14 @@
+module Spree::BaseHelper
+  def link_to_cart(text = t('cart'))
+    return "" if current_page?(cart_path)
+    css_class = nil
+    if current_order.nil? or current_order.line_items.empty?
+      text = "#{text}: (#{t('empty')})"
+      css_class = 'empty'
+    else
+      text = "#{text}: (#{current_order.item_count})"
+      css_class = 'full'
+    end
+    link_to text, cart_path, :class => css_class
+  end
+end

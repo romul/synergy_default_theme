37f11110a013c35e1391ecb9855ef8be22d0d713
Alexander Shuhin <a.medium@gmail.com>
2011-03-29 13:50:30 +0400

    Перенес настройки размера изображения в Activate, подправил стили для иконок на странице товара

--- lib/synergy_default_theme.rb
+++ lib/synergy_default_theme.rb
@@ -10,6 +10,15 @@ module SynergyDefaultTheme
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
+
+      Image.attachment_definitions[:attachment].merge!({
+        :styles => {
+                :mini    => '40x40>',
+                :small   => '120x120>',
+                :product => '200x200>',
+                :large   => '600x600>'
+        }
+      })
     end
 
     config.to_prepare &method(:activate).to_proc

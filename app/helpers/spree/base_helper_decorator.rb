9ebe27c09367f94e1070a2b53a8237aa4c486a84
Alexander Shuhin <a.medium@gmail.com>
2011-03-29 12:30:53 +0400

    Изменил размеры изображений :small и :product, изменил шаблон представления таксономий, небольшие изменения в представлении товара

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -11,4 +11,15 @@ module Spree::BaseHelper
     end
     link_to text, cart_path, :class => css_class
   end
+
+  def format_price(price, options={})
+    options.assert_valid_keys(:show_vat_text)
+    options.reverse_merge! :show_vat_text => Spree::Config[:show_price_inc_vat]
+    formatted_price = number_to_currency(price, :precision => 0)
+    if options[:show_vat_text]
+      I18n.t(:price_with_vat_included, :price => formatted_price)
+    else
+      formatted_price
+    end
+  end
 end

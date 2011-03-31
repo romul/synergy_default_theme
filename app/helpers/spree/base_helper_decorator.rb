a4f7792a553da174d1655cddaea1ebbdc7874ee6
Alexander Shuhin <a.medium@gmail.com>
2011-03-31 15:56:46 +0400

    Переверстана карзина товаров

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -12,6 +12,19 @@ module Spree::BaseHelper
     link_to text, cart_path, :class => css_class
   end
 
+  def order_price(order, options={})
+    options.assert_valid_keys(:format_as_currency, :show_vat_text, :show_price_inc_vat)
+    options.reverse_merge! :format_as_currency => true, :show_vat_text => true
+
+    # overwrite show_vat_text if show_price_inc_vat is false
+    options[:show_vat_text] = Spree::Config[:show_price_inc_vat]
+
+    amount =  order.item_total
+    amount += Calculator::Vat.calculate_tax(order) if Spree::Config[:show_price_inc_vat]
+
+    options.delete(:format_as_currency) ? number_to_currency(amount, :precision => 0) : amount
+  end
+
   def format_price(price, options={})
     options.assert_valid_keys(:show_vat_text)
     options.reverse_merge! :show_vat_text => Spree::Config[:show_price_inc_vat]

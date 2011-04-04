42848e7f53d3ae00975847400ae057d6a195e3b4
Alexander Shuhin <a.medium@gmail.com>
2011-04-04 12:38:04 +0400

    Адаптация spree_reviews к synergy_default_theme

---
+++ lib/spree_reviews_hook.rb
@@ -0,0 +1,16 @@
+class SpreeReviewsHooks < Spree::ThemeSupport::HookListener
+  insert_after :inside_head do
+    %(<%= stylesheet_link_tag('reviews.css') %>)
+  end
+
+  insert_after :admin_product_sub_tabs do
+    %(
+       <%= tab(:reviews, :label => 'review_management') %>
+     )
+  end
+
+  insert_after :admin_configurations_menu do
+    %(<%= configurations_menu_item(t('reviews.review_settings'), admin_review_settings_path, t('reviews.manage_review_settings')) %>)
+  end
+
+end

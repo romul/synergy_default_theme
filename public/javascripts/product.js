e5a9b4242f119b772335302e95c44a83373f052b
Alexander Shuhin <a.medium@gmail.com>
2011-04-25 14:19:34 +0400

    Адаптирован модуль spree_enhanced_option_types

---
+++ public/javascripts/product.js
@@ -0,0 +1,22 @@
+var add_image_handlers = function() {
+  $("#main-image").data('selectedThumb', $('#main-image img').attr('src'));
+  $('ul.thumbnails li').eq(0).addClass('selected');
+  $('ul.thumbnails li a').click(
+    function() {
+      $("#main-image").data('selectedThumb', $(this).attr('href'));
+      $('ul.thumbnails li').removeClass('selected');
+      $(this).parent('li').addClass('selected');
+      return false;
+    }).hover(
+    function() {
+      $('#main-image img').attr('src', $(this).attr('href').replace('mini', 'product'));
+    },
+    function() {
+      $('#main-image img').attr('src', $("#main-image").data('selectedThumb'));
+    }
+  );
+};
+
+$(function() {
+  variant_changed(default_variant_id);
+});

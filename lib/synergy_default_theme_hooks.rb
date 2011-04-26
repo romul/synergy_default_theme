921005cac7fa0ff3e9a5e49725340d75bf3f51a6
Alexander Shuhin <a.medium@gmail.com>
2011-04-26 17:58:16 +0400

    Произведена работа над кнопками в корзине

--- lib/synergy_default_theme_hooks.rb
+++ lib/synergy_default_theme_hooks.rb
@@ -1,3 +1,5 @@
 class SynergyDefaultThemeHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_inside_taxonomy_form, 'admin/taxonomies/show_on_homepage_field'
+
+  remove :continue_shopping_or_empty_cart
 end

898b66df828a4a41ff7b2c6f42c5186cea108b7c
Alexander Shuhin <a.medium@gmail.com>
2011-04-28 14:47:35 +0400

    Реализация всплывающего окна на изображении товара

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -1,18 +1,11 @@
 var add_image_handlers = function() {
-  $("#main-image").data('selectedThumb', $('#main-image img').attr('src'));
   $('ul.thumbnails li').eq(0).addClass('selected');
   $('ul.thumbnails li a').click(
     function() {
-      $("#main-image").data('selectedThumb', $(this).attr('href'));
+      $('#main-image img').attr('src', $(this).attr('href'));
+      $('#main-image a').attr('href', $(this).attr('href'));
       $('ul.thumbnails li').removeClass('selected');
       $(this).parent('li').addClass('selected');
       return false;
-    }).hover(
-    function() {
-      $('#main-image img').attr('src', $(this).attr('href').replace('mini', 'product'));
-    },
-    function() {
-      $('#main-image img').attr('src', $("#main-image").data('selectedThumb'));
-    }
-  );
+    });
 };

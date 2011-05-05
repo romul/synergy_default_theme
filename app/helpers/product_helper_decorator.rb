697f3edb5fb2cb67b5b5a2a176387dba266d14b2
Alexander Shuhin <a.medium@gmail.com>
2011-05-05 14:45:05 +0400

    При добавлении стоимости для товара отображается теперь "+" вместо добавить

--- app/helpers/product_helper_decorator.rb
+++ app/helpers/product_helper_decorator.rb
@@ -1,6 +1,16 @@
 ProductsHelper.module_eval do
+  def variant_price_diff(variant)
+    return product_price(variant) unless variant.product.master.price
+    diff = product_price(variant, :format_as_currency => false) - product_price(variant.product, :format_as_currency => false)
+    return nil if diff == 0
+    if diff > 0
+      "(+ #{format_price diff.abs})"
+    else
+      "(- #{format_price diff.abs})"
+    end
+  end
+
   def product_description(product)
     raw(product.description)
   end
-
 end

1b1bcfdfd3534b9358c26f6576e4673660c4b5f6
Roman Smirnov <roman@railsdog.com>
2011-06-06 20:18:33 +0400

    Removed caching taxon objects from homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,7 @@ class HomeController < Spree::BaseController
   helper :products, :taxons
 
   def index
-    @taxons = Rails.cache.fetch('homepage-taxons', :expires_in => LONG_INTERVAL) do
-      home_taxonomy = get_taxonomies.find{|t| t.show_on_homepage? }
-      home_taxonomy ? home_taxonomy.root.children : []
-    end
+    home_taxonomy = get_taxonomies.find{|t| t.show_on_homepage? }
+    @taxons = home_taxonomy ? home_taxonomy.root.children : []
   end
 end
